756d18d98ec4fa16f52a68fb76d950f1852b5320
suro <timu.zaxarov@gmail.com>
2021-05-14 19:48:11 +0900

    fix vote

--- JustBot/src/main/java/ru/rien/bot/modules/dota2/ModuleDota2.java
+++ JustBot/src/main/java/ru/rien/bot/modules/dota2/ModuleDota2.java
@@ -16,6 +16,7 @@ import ru.rien.bot.objects.GuildWrapper;
 import ru.rien.bot.schedule.JustBotTask;
 import ru.rien.bot.utils.MessageUtils;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,12 +37,18 @@ public class ModuleDota2 extends ModuleDiscord {
                     long guildid = vote.guildid;
                     GuildWrapper guildWrapper = getModuleDsBot().getManager().getGuildNoCache(guildid+"");
                     TextChannel textChannel = guildWrapper.getGuild().getTextChannelById(vote.getChannelid());
+                    long vote_time = vote.start_time/1000;
+                    if(new Date().getTime()/1000 - vote_time > 3600){
+                        matchscan.remove(vote);
+                        return;
+                    }
                     if(textChannel != null) {
                         try {
                             HttpResponse<JsonNode> response = Unirest.get("https://api.opendota.com/api/players/" + aLong + "/matches?limit=1").asJson();
                             JSONObject jsonObject = response.getBody().getArray().getJSONObject(0);
                             long start_time = jsonObject.getLong("start_time");
-                            long vote_time = vote.start_time/1000;
+
+
                             if(vote_time<start_time || vote_time-600<start_time) {
                                 int slot = jsonObject.getInt("player_slot");
                                 if (jsonObject.getBoolean("radiant_win") && slot <= 127) {
