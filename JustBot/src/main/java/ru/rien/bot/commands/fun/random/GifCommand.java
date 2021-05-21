8a9216b3b97ad1c6d34af087ffc6342e4a412c98
suro <timu.zaxarov@gmail.com>
2021-05-21 17:25:41 +0900

    maybe fix embed gif

--- JustBot/src/main/java/ru/rien/bot/commands/fun/random/GifCommand.java
+++ JustBot/src/main/java/ru/rien/bot/commands/fun/random/GifCommand.java
@@ -4,6 +4,7 @@ import com.google.common.base.Joiner;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.springframework.stereotype.Component;
 import ru.rien.bot.modules.command.Command;
 import ru.rien.bot.modules.command.CommandEvent;
@@ -11,6 +12,7 @@ import ru.rien.bot.modules.command.CommandType;
 import ru.rien.bot.modules.messsage.Language;
 import ru.rien.bot.objects.GuildWrapper;
 import ru.rien.bot.permission.Permission;
+import ru.rien.bot.utils.MessageUtils;
 
 @Component
 public class GifCommand implements Command {
@@ -23,7 +25,11 @@ public class GifCommand implements Command {
                 tags = "&tag=" + Joiner.on("+").join(args);
             }
             HttpResponse<JsonNode> response = Unirest.get("http://api.giphy.com/v1/gifs/random?api_key=mSJjgY9ZkbDtu5ON9WYGK6txrwAAFaX5" + tags).asJson();
-            event.getChannel().sendMessage(response.getBody().getObject().getJSONObject("data").getString("url")).queue();
+            MessageEmbed embed = MessageUtils.getEmbed(event.getSender()).
+                    setImage(response.getBody().getObject().getJSONObject("data").getString("url"))
+                    .build();
+            event.getChannel().sendMessage(embed).queue();
+//            event.getChannel().sendMessage().queue();
         } catch (Exception ignored) {
             //this exception is about as useful as a nipple on a male
         }
